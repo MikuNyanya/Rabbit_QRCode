34ad0bdde14b7af6e759d20edf061cdee21c7fc1
MikuNyanya <k19831221@163.com>
2021-12-15 17:38:37 +0800

    init

---
+++ src/main/java/cn/mikulink/rabbitqrcode/Main.java
@@ -0,0 +1,30 @@
+package cn.mikulink.rabbitqrcode;
+
+import java.awt.*;
+
+/**
+ * created by MikuNyanya on 2021/12/15 15:42
+ * For the Reisen
+ * 使用演示
+ */
+public class Main {
+    public static void main(String[] args) {
+        //二维码长度
+        int width = 300;
+        //二维码宽度
+        int height = 300;
+        //二维码背景颜色
+        Color onColor = new Color(Integer.parseInt("000000",16));
+        //二维码前景颜色
+        Color offColor = new Color(Integer.parseInt("66ccff",16));
+
+
+        //以最简单的调用方法
+        //生成一个普通黑白二维码
+
+
+
+
+
+    }
+}
---
+++ src/test/java/cn/mikulink/rabbitqrcode/QRCodeServiceTest.java
@@ -0,0 +1,31 @@
+package cn.mikulink.rabbitqrcode;
+
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * created by MikuNyanya on 2021/12/6 11:44
+ * For the Reisen
+ */
+public class QRCodeServiceTest {
+
+    @Test
+    public void test() {
+        try {
+            BufferedImage qrImg = QRCodeService.createGradientColorQRCode("https://github.com/MikuNyanya/RabbitBot_RE",
+                    500, 500,
+                    new Color(Integer.parseInt("FF96A0", 16)), new Color(Integer.parseInt("FF69B4", 16)),
+                    new Color(Integer.parseInt("FFFFFF", 16)),
+                    "https://i1.hdslb.com/bfs/face/d973f5f34a9b102a9a0d6f216f806c225d66912f.jpg");
+
+            ImageIO.write(qrImg, "png", new File("E:\\qrImage.png"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
